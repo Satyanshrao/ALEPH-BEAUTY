468f6b6614d979a2df474f3689febf247610a6b7
satyanshrao <sarthakyadav730@gmail.com>
2024-07-14 22:39:02 +0530

    commit 2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var main =document.querySelector("#main")
 var cursor = document.querySelector("#cursor")
+var email =document.querySelector("#youremail")
+var innput =document.querySelector("#innput")
 
 main.addEventListener("mousemove",function(dets){
     cursor.style.left =dets.x+"px"
     cursor.style.top =dets.y+"px"
+})
+innput.addEventListener("click",function(){
+    email.style.display = "none"
 })
